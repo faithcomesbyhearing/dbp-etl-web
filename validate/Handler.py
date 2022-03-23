5012ca488deb344ab9830496b9646b57a320c300
Brad Flood <bflood@keyholesoftware.com>
2022-03-23 17:08:02 -0500

    code in source control / consistency

--- validate/Handler.py
+++ validate/Handler.py
@@ -9,12 +9,16 @@ from PreValidate import *
 
 
 def handler(event, context):
-	session = boto3.Session()
-	s3 = session.client("s3")
-	print("Copying lpts-dbp.xml...")
-	s3.download_file(os.getenv("UPLOAD_BUCKET"), "lpts-dbp.xml", "/tmp/lpts-dbp.xml")
-	lptsReader = LPTSExtractReader("/tmp/lpts-dbp.xml")
 	directory = event["prefix"] # can be filesetId or lang_stockno_USX
 	filenames = event["files"] # Should be object keys
-	messages = PreValidate.validateLambda(lptsReader, directory, filenames)
+	bucket    = event["bucket"] # Should be bucketname without s3://
+
+	session = boto3.Session()
+	s3Client = session.client("s3")
+	print("Copying lpts-dbp.xml...")
+	s3Client.download_file(os.getenv("UPLOAD_BUCKET"), "lpts-dbp.xml", "/tmp/lpts-dbp.xml")
+	lptsReader = LPTSExtractReader("/tmp/lpts-dbp.xml")
+	
+	preValidate = PreValidate(lptsReader, UnicodeScript(), s3Client, bucket)
+	messages = preValidate.validateLambda(lptsReader, directory, filenames)
 	return messages
