dad0cbfd6014f86bfbbcbe7a02f215fdd364ca30
Brad Flood <bflood@keyholesoftware.com>
2022-03-24 11:36:38 -0500

    move validate and postvalidate to dbp-etl repo

--- postvalidate/Handler.py
+++
@@ -1,9 +0,0 @@
-from ValidatePage import *
-
-def handler(event, context):
-	page = ValidatePage()
-	hashIdList = page.getHashIds(None, event["filesetId"])
-	page.process(None, hashIdList)
-	page.close()
-	with open('/tmp/sample.html') as f:
-	    return f.read()
